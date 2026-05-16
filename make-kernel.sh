b594a16c61120052a9293a8fca0c5831c3c42783
Sean Donnellan <darkdonno@gmail.com>
2026-05-16 17:52:34 +0930

    Set-up GitHub Action to build Linux Kernel for QEMU's microvm machine type.

--- make-kernel.sh
+++ make-kernel.sh
@@ -3,14 +3,23 @@
 #
 # podman run --rm -it -v .:/work --workdir /work public.ecr.aws/docker/library/alpine:3.22.4
 
+VERSION=6.18
 echo Install packages required to configure and build kernel
-apk add --no-cache alpine-sdk flex bison gawk bc ncurses-dev elfutils-dev git || exit 1
-# git - for cloning the Linux kernel. The alternative would be to use a tar
-# elfutils-dev - needed for gelf.h
+apk add --no-cache alpine-sdk flex bison gawk bc ncurses-dev elfutils-dev git linux-headers || exit 1
+# flex - the build system generates lexical analysers during build.
+# bison - the build system generates parsers during build
+# gawk - needs it.
 # ncurses-dev - needed by "make menuconfig"
+# elfutils-dev - needed for gelf.h
+# git - for cloning the Linux kernel. The alternative would be to use a tar
+# linux-headers - for "asm/types.h" and other headers required.
 
 echo Cloning Linux kernel repository
-git clone --depth 1 --branch "v6.18" https://github.com/torvalds/linux /linux || exit 1
+git clone --depth 1 --branch "v$VERSION" https://github.com/torvalds/linux /linux || exit 1
+
+start=$(pwd)
+cp microvm.config /linux/.config
+cd /linux
 
 if [ -f .config ]; then
     echo Using existing .config
@@ -20,4 +29,16 @@ else
     # Configure
     make menuconfig
 fi
+
+echo Building the Linux Kernel.
 make -j4
+if [ $? != 0 ];
+then
+    echo "Failed to build the Linux Kernel."
+    exit 2
+fi
+
+cp arch/x86/boot/bzImage $(start)/microvm.bzImage &&  \
+    cp .config $(start)/microvm.config && \
+    cp System.map $(start)/microvm.System.map && \
+    echo "Copied kernel files" || exit 3
